27c3b27b1464b77a3b96f18f20f5808028aaeb9e
developerbrm <developerbrm@gmail.com>
2025-03-10 18:01:07 +0530

    - express server created

---
+++ server/index.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosResponse } from 'axios'
+import express from 'express'
+import { ImagesAPIResponse, PexelsApiResponse } from '../models'
+import { API_ROUTES } from '../utilities'
+
+const app = express()
+const API_KEY = import.meta.env.VITE_API_KEY
+const baseUrl = 'https://api.pexels.com/v1/search'
+const port = 3000
+
+app.get(
+  API_ROUTES['images'],
+  async (req: express.Request, res: express.Response) => {
+    const queryString = new URLSearchParams({
+      query: 'indian',
+      orientation: 'portrait',
+    }).toString()
+
+    const config = {
+      method: 'GET',
+      maxBodyLength: Infinity,
+      headers: {
+        Authorization: API_KEY,
+      },
+    }
+
+    const finalURL = `${baseUrl}/?${queryString}`
+
+    axios
+      .get(finalURL, config)
+      .then((response: AxiosResponse<PexelsApiResponse>) => {
+        const data = response.data
+        const images: ImagesAPIResponse[] = data.photos.map((image) => ({
+          srcArr: image.src,
+          alt: image.alt,
+        }))
+
+        res.status(200).send({ images })
+      })
+      .catch((error) => {
+        console.log(error)
+
+        res.status(500).send({
+          message: 'Something went wrong',
+          images: [],
+        })
+      })
+  }
+)
+
+app.listen(port, () => {
+  console.log(`Started server on ${port}`)
+})
