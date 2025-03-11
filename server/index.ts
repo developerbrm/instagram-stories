153a3c6ea15bfcde1b3be46851f8cdb8eb2ec944
developerbrm <developerbrm@gmail.com>
2025-03-11 12:09:34 +0530

    - cors issue fixes

--- server/index.ts
+++ server/index.ts
@@ -1,57 +1,64 @@
 import axios, { AxiosResponse } from 'axios'
 import express from 'express'
 import { ImagesAPIResponse, PexelsApiResponse } from '../models'
-import { SERVER_API_ROUTES } from '../utilities'
+import { getErrorMessage, SERVER_API_ROUTES } from '../utilities'
+import cors from 'cors'
 
 const app = express()
 const API_KEY = import.meta.env.VITE_API_KEY
 const baseUrl = 'https://api.pexels.com/v1/search'
 const port = 3000
 
-app.get(
-  SERVER_API_ROUTES['images'],
-  async (req: express.Request, res: express.Response) => {
-    const defaultQueryObj = {
-      query: 'people',
-      orientation: 'portrait',
-    }
+const corsOptions = {
+  origin: import.meta.env.VITE_CORS_ALLOW_ORIGIN,
+}
 
-    const queryString = new URLSearchParams({
-      ...defaultQueryObj,
-      ...req.query,
-    }).toString()
+app
+  .use(cors(corsOptions))
+  .get(
+    SERVER_API_ROUTES['images'],
+    async (req: express.Request, res: express.Response) => {
+      const defaultQueryObj = {
+        query: 'people',
+        orientation: 'portrait',
+      }
 
-    const config = {
-      method: 'GET',
-      maxBodyLength: Infinity,
-      headers: {
-        Authorization: API_KEY,
-      },
-    }
+      const queryString = new URLSearchParams({
+        ...defaultQueryObj,
+        ...req.query,
+      }).toString()
 
-    const finalURL = `${baseUrl}/?${queryString}`
+      const config = {
+        method: 'GET',
+        maxBodyLength: Infinity,
+        headers: {
+          Authorization: API_KEY,
+        },
+      }
 
-    axios
-      .get(finalURL, config)
-      .then((response: AxiosResponse<PexelsApiResponse>) => {
-        const data = response.data
-        const images: ImagesAPIResponse[] = data.photos.map((image) => ({
-          srcArr: image.src,
-          alt: image.alt,
-        }))
+      const finalURL = `${baseUrl}/?${queryString}`
 
-        res.status(200).send({ images })
-      })
-      .catch((error) => {
-        console.log(error)
+      axios
+        .get(finalURL, config)
+        .then((response: AxiosResponse<PexelsApiResponse>) => {
+          const data = response.data
+          const images: ImagesAPIResponse[] = data.photos.map((image) => ({
+            srcArr: image.src,
+            alt: image.alt,
+          }))
 
-        res.status(500).send({
-          message: 'Something went wrong',
-          images: [],
+          res.status(200).send({ images })
         })
-      })
-  }
-)
+        .catch((error) => {
+          console.log(error)
+
+          res.status(500).send({
+            message: getErrorMessage(error),
+            images: [],
+          })
+        })
+    }
+  )
 
 app.listen(port, () => {
   console.log(`Started server on ${port}`)
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string
+  readonly VITE_CORS_ALLOW_ORIGIN: string
 }
 
 interface ImportMeta {
